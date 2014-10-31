b50cde60c772db9163f71f643022c3d33cc183cf
Gerald Bauer <gerald.bauer@gmail.com>
2014-10-31 20:08:09 +0100

    Create world.js

---
+++ js/world.js
@@ -0,0 +1,24 @@
+
+var AT = {
+  title: 'Austria',
+  latLng: [47.2000338, 13.199959],
+  zoom:    7,   // default zoom level (for overview) e.g.  use 7 for austria
+
+  N: {   // province N - Niederösterreich
+    latLng: [48.2817813, 15.7632457],
+    zoom:   8  // default zoom level (for overview) e.g.  use 8 for province
+  },
+
+  states: {  // NOTE: use AT NUTS sort order for states
+    'Burgenland': 11,
+    'Niederösterreich': 12,
+    'Wien': 13,
+    'Kärnten': 21,
+    'Steiermark': 22,
+    'Oberösterreich': 31,
+    'Salzburg': 32,
+    'Tirol': 33,
+    'Vorarlberg': 34
+  }
+};
+
